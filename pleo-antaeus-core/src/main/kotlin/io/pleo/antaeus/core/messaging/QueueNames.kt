2e9eae536008fe34da5bfea4afb6ad7e2eec8cf0
Mireya Rodriguez <mrsa@kamstrup.com>
2022-03-02 16:46:37 +0000

    Add QueueNames file for constants

---
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/messaging/QueueNames.kt
@@ -0,0 +1,8 @@
+package io.pleo.antaeus.core.messaging
+
+class QueueNames {
+    companion object {
+        val pendingInvoicesQueueName : String = "PendingInvoicesQueue"
+        val DueInvoicesQueue : String = "DueInvoicesQueue"
+    }
+}
