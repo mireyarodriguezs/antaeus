75966ca567ec23b318228ed1bb7bf703625889bf
Mireya Rodriguez <mrsa@kamstrup.com>
2022-03-02 16:45:18 +0000

    Rename class PendingInvoicesMessageConsumer

--- pleo-antaeus-messageconsumer/src/main/kotlin/io/pleo/antaeus/messageconsumer/PendingInvoicesMessageConsumer.kt
+++ pleo-antaeus-messageconsumer/src/main/kotlin/io/pleo/antaeus/messageconsumer/PendingInvoicesMessageConsumer.kt
@@ -3,23 +3,23 @@ package io.pleo.antaeus.messageconsumer
 import io.pleo.antaeus.core.CoroutineFactory
 import io.pleo.antaeus.core.messaging.QueueMessageConsumer
 import io.pleo.antaeus.core.services.BillingService
+import io.pleo.antaeus.core.messaging.QueueNames
 import kotlinx.coroutines.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
-private const val queueName = "PendingInvoicesQueue"
 private const val consumerTag = "SingleConsumer"
 
-class MessageConsumer(private val billingService: BillingService) {
+class PendingInvoicesMessageConsumer(private val billingService: BillingService) {
 
     fun listen() {
         CoroutineFactory.create(nThreads = 4, block = {
-            println("Creating message consumer....")
-            val messageConsumer = QueueMessageConsumer(queueName, consumerTag)
+            println("Creating PendingInvoicesMessageConsumer message consumer....")
+            val messageConsumer = QueueMessageConsumer(QueueNames.pendingInvoicesQueueName, consumerTag)
             val deliverCallback = { messageStr: String? ->
                 println("Received new message with content $messageStr")
                 val idList = Json.decodeFromString<List<Int>>(messageStr.toString())
-                runBlocking() {
+                runBlocking {
                     billingService.settleForIds(idList.toList())
                 }
             }
