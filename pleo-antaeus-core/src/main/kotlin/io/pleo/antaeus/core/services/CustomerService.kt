019a5f239e94ecfcb6ee03bc48921d09bb975847
Mireya Rodriguez <mrsa@kamstrup.com>
2022-03-02 16:47:25 +0000

    Extend CustomerService with hasActiveSubscription and changeSubscriptionStatus operations

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/CustomerService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/CustomerService.kt
@@ -4,9 +4,11 @@
 
 package io.pleo.antaeus.core.services
 
+import com.sun.org.apache.xpath.internal.operations.Bool
 import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
 import io.pleo.antaeus.data.CustomerDal
 import io.pleo.antaeus.models.Customer
+import io.pleo.antaeus.models.SubscriptionStatus
 
 class CustomerService(private val dal: CustomerDal) {
     fun fetchAll(): List<Customer> {
@@ -16,4 +18,10 @@ class CustomerService(private val dal: CustomerDal) {
     fun fetch(id: Int): Customer {
         return dal.fetchCustomer(id) ?: throw CustomerNotFoundException(id)
     }
+
+    fun hasActiveSubscription(id: Int) : Boolean =
+        dal.fetchCustomer(id)?.subscriptionStatus == SubscriptionStatus.ACTIVE
+
+    fun changeSubscriptionStatus(id: Int, subscriptionStatus: SubscriptionStatus) =
+        dal.changeSubscriptionStatus(id, subscriptionStatus)
 }
